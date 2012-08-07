6cc457ca9499e2395a225a6552ea1fc29c511530
Roy Meijerman <roy_m86@hotmail.com>
2012-08-07 14:54:40 +0200

    added some variables

--- define.php
+++ define.php
@@ -1,5 +1,11 @@
 <?php
-define( "HOST", "localhost" );
+define( "HOST_NAME", "localhost" );
+define( "USER_NAME", "" );
+define( "PASSWORD", "" );
+define( "DATABASE", "" );
+
+define( "SITE_NAME", "" );
+define( "SITE_URL", "" );
 
 include( "classes.php" );
 ?>
